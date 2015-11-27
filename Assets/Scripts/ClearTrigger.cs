828274b80569285c26c0aac806927d97c4b26d44
Ryo Yamamoto <ryo24.yamamoto@gmail.com>
2015-11-28 02:52:28 +0900

    Add ClearTrigger

---
+++ Assets/Scripts/ClearTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClearTrigger : MonoBehaviour {
+	GameObject gameController;
+	// Use this for initialization
+	void Start () {
+		gameController = GameObject.FindWithTag("GameManager");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		gameController.SendMessage("IncreaseScore");
+	}
+}
